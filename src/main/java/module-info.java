93b775153c37cf9847f2dbf3f98f4e29fa25e144
Hugojavid5 <hugojavidferrero@gmail.com>
2025-01-05 16:33:20 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.hugo.dein.jasperejercicio4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.hugo.dein.jasperejercicio4 to javafx.fxml;
+    exports org.hugo.dein.jasperejercicio4;
+}
---
+++ src/main/java/org/hugo/dein/jasperejercicio4/HelloController.java
@@ -0,0 +1,14 @@
+package org.hugo.dein.jasperejercicio4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
