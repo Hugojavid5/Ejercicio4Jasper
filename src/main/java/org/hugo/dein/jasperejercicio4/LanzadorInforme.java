411e6855dba17a598c3a5edec9cf5882aeb48555
Hugojavid5 <hugojavidferrero@gmail.com>
2025-01-07 17:46:50 +0100

    ejercicio terminado sin javadocs

---
+++ src/main/java/org/hugo/dein/jasperejercicio4/LanzadorInforme.java
@@ -0,0 +1,38 @@
+package org.hugo.dein.jasperejercicio4;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Clase principal de la aplicación que inicia la interfaz gráfica.
+ */
+public class LanzadorInforme extends Application {
+
+    /**
+     * Configura y muestra la ventana principal de la aplicación.
+     *
+     * @param stage la ventana principal (Stage) proporcionada por JavaFX.
+     * @throws IOException si ocurre un error al cargar el archivo FXML.
+     */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LanzadorInforme.class.getResource("/fxml/interfaz.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Formulario medico!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * Punto de entrada principal de la aplicación.
+     *
+     * @param args argumentos de la línea de comandos (no utilizados en esta aplicación).
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+}
